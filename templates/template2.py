4e4becb8fa7e7a47986571b0f54b3a8330def9de
eveiramirez <ignemmram@gmail.com>
2021-05-26 21:22:47 -0500

    Creacion del template2

---
+++ templates/template2.py
@@ -0,0 +1,38 @@
+"""
+
+NAME
+    [name].py
+
+VERSION
+    [#.#]
+
+AUTHOR
+    [Author] <[username]@gmail.com>
+
+CONTACT
+    [email]
+
+GITHUB
+    [GitHub link]
+
+DESCRIPTION
+    [Briefly describe what the program does]
+
+CATEGORY
+    [Category of the program: sequence analysis for example]
+
+USAGE
+    [name].py  [-options/arguments]
+
+ARGUMENTS
+    [name or flags]  [description]
+    ...
+
+FUNCTIONS
+    [name]  [parameters]  [description]
+    ...
+
+EXAMPLES
+    [Example 1: describe the example, input and outputs]
+
+"""
