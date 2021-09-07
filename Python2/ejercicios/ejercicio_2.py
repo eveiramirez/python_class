536a6bc50b89d18a6705ab123a15447be5a08c76
Pou <ignemmram@gmail.com>
2021-09-07 18:02:59 -0500

    tarea 1 actualizada y ejercicio 2

--- Python2/ejercicios/ejercicio_2.py
+++ Python2/ejercicios/ejercicio_2.py
@@ -1,3 +1,5 @@
+import re
+
 from Bio.Seq import Seq
 from Bio.SeqUtils import nt_search
 from Bio import SeqIO
@@ -14,5 +16,19 @@ for i in range(1, len(start)):
     proteina = sec_prot.translate(to_stop=True)
     print(proteina)
 
-##Ejercicio 2
-filename = "../../src/class2/"
+# Ejercicio 2
+filename = "../../src/class2/seq.nt.fa"
+id_dict = SeqIO.to_dict(SeqIO.parse(filename, "fasta"))
+
+for i in id_dict:
+    print(">{}".format(i))
+    sec = id_dict[i].seq
+    for codon in re.findall(r".{3}", str(sec[1:len(sec)])):
+        print(codon, end="\t")
+    print("\n")
+
+# Ejercicio 3
+filename = "../../src/class2/sample.fastq"
+
+for record in SeqIO.parse(filename, "fastq"):
+    print(record.letter_annotations)
--- python2/tareas/tarea_1.py
+++ python2/tareas/tarea_1.py
@@ -1,7 +1,67 @@
-class Pol_Regular(object):
+"""
 
-    def __init__(self, nombre: str, no_lados: int, tam_lados: int):
-        self.nombre = nombre
+NAME
+        tarea_1.py
+
+VERSION
+        [1.0]
+
+AUTHOR
+        Ignacio Emmanuel Ramirez Bernabe
+
+CONTACT
+        iramirez@lcg.unam.mx
+
+GITHUB
+        https://github.com/eveiramirez/python_class/blob/master
+        /python_scripts/tarea_1.py
+
+DESCRIPTION
+        Este programa contiene la clase PolRegular, que permite crear
+        objetos que tengan atributos de poligonos regulares y obtener
+        informacion acerca de ellos.
+
+        Tambien se encuentran las subclases Cuadrado y Triangulo, las
+        cuales contienen metodos mas especificos
+
+CATEGORY
+        Math
+        Regular Polygon
+        POO
+
+"""
+
+
+class PolReg(object):
+    """
+    Clase para la creacion de poligonos regulares.
+
+    Atributos
+    ---------
+    no_lados: Numero de lados
+
+    tam_lados: Tamano de los lados
+
+    Metodos
+    -------
+    sum_ang_int(self)
+        Obtiene la suma de angulos interiores en grados sexagesimales.
+
+    ang_cent(self)
+        Obtiene el valor del angulo central en grados sexagesimales.
+
+    ang_int(self)
+        Obtiene el valor del angulo interior en grados sexagesimales.
+
+    ang_ext(self)
+        Obtiene el valor del angulo exterior en grados sexagesimales.
+
+    perimetro(self)
+        Obtiene el perimetro.
+
+    """
+
+    def __init__(self, no_lados: float, tam_lados: float):
         self.no_lados = no_lados
         self.tam_lados = tam_lados
 
@@ -25,22 +85,72 @@ class Pol_Regular(object):
         p = self.no_lados*self.tam_lados
         return p
 
-    def area(self, apotema: int):
-        a = (self.perimetro()*apotema)/2
-        return a
 
+class Cuadrado(PolReg):
+    """
+    Clase para la creacion de cuadrados.
 
-class Cuadrado(Pol_Regular):
+    Metodos
+    -------
+    area(self)
+        Obtiene el valor del area.
+
+    """
+
+    def __init__(self, tam_lados):
+        PolReg.__init__(self, no_lados=4, tam_lados=tam_lados)
+
+    def sum_ang_int(self):
+        return 360
+
+    def ang_cent(self):
+        return 90
+
     def ang_int(self):
         return 90
 
     def ang_ext(self):
         return 90
 
-    def ang_cent(self):
-        return 90
+    def perimetro(self):
+        p = self.no_lados*4
+        return p
+
+    def area(self):
+        a = self.tam_lados**2
+        return a
 
 
-class Triangulo(Pol_Regular):
+class Triangulo(PolReg):
+    """
+        Clase para la creacion de cuadrados.
+
+        Metodos
+        -------
+        area(self)
+            Obtiene el valor del area.
+
+    """
+
+    def __init__(self, tam_lados):
+        PolReg.__init__(self, no_lados=3, tam_lados=tam_lados)
+
     def sum_ang_int(self):
         return 180
+
+    def ang_cent(self):
+        return 120
+
+    def ang_int(self):
+        return 60
+
+    def ang_ext(self):
+        return 120
+
+    def perimetro(self):
+        p = self.no_lados * 3
+        return p
+
+    def area(self):
+        a = (3**.5)*(self.tam_lados**2)/4
+        return a
